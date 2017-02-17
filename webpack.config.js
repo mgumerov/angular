99d6aa257a3f3de0542c50137312fd05550b0448
Maksim Gumerov <maxim.gumerov@massmo.ru>
2017-02-17 12:18:03 +0500

    Попытка миграции на webpack, безуспешная

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    //module: {
+//        loaders: [
+//          {
+//            test: /\.tsx?$/,
+//            loader: 'awesome-typescript-loader'
+//          }
+//        ]
+//    },
+//    resolve: {
+//        extensions: ['.ts','.js']
+//    },
+// Source maps support ('inline-source-map' also works)
+    devtool: 'source-map'
+}
